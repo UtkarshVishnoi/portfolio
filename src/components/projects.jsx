2c388a6f580b6f1dfc1fb69de401e86494598479
Utkarsh Vishnoi <utkarsh.v8274@gmail.com>
2024-03-15 22:00:10 +0530

    Fixed minor responsive issue

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,6 +4,7 @@ export default function Projects() {
   return (
     <div id="main-project-div">
       <h1 className="div-header-text">{"<projects>"}</h1>
+      <div id="projects-div"></div>
       <h1 className="div-header-text">{"</projects>"}</h1>
     </div>
   );
