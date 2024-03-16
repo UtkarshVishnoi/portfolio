f2acee989180e7fd0c550f7171984d71c2438455
Utkarsh Vishnoi <utkarsh.v8274@gmail.com>
2024-03-16 22:51:59 +0530

    Added the download CV button

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,6 +4,10 @@ export default function Contact() {
   return (
     <div id="main-contact-div">
       <h1 className="div-header-text">{"<contact>"}</h1>
+      <div className="contact-details-div"></div>
+      <div className="cv-download-div">
+        <button id="cv-download-btn">Download CV</button>
+      </div>
       <h1 className="div-header-text">{"</contact>"}</h1>
     </div>
   );
